866fe7943c7c2b91163efd04ea44b0c1001810f3
vitor1aSj <vitoria.vih.silvah@gmail.com>
2026-07-01 21:22:46 -0300

    Sistema de Vendas

---
+++ ProjetoFinal.py
@@ -0,0 +1,51 @@
+
+#SISTEMA DE VENDAS
+
+estoque_produtos = {
+ 1:{"Nome": "Camiseta de banda Blade and Bath", "preco":110.00, "quantidade": 20},
+ 2:{"Nome": "Camiseta de banda Decalius", "preco":100.00, "quantidade":17},
+ 3:{"Nome": "Camiseta de banda Made Of Pain", "preco":99.99, "quantidade":50},
+ 4:{"Nome": "Camiseta de banda Type O negative", "preco":110.00, "quantidade":33}
+}
+
+carrinho = {}
+
+while True:
+    print("💀💀💀💀💀💀💀💀💀💀💀")
+    print("Seja bem vindo!")
+    print("💀💀💀💀💀💀💀💀💀💀💀")
+    print(" [1] Vizualizar estoque.")
+    print(" [2] Adicionar item ao carrinho.")
+    print(" [3] Vizualizar carrinho.")
+    print(" [4] Finalizar compra.")
+    print(" [5] Sair do sistema.")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao == 1:
+        print("Vizualizando Estoque!")
+        print("ID | NOME | VALOR | QUANTIDADE")
+        for chave, valor in estoque_produtos.items():
+            print(f"{chave}| {valor}")
+
+    elif opcao == 2:
+        print("Adicionando itens ao carrinho!")
+        id_produto = int(input("Qual ID do produto você deseja comprar? "))
+        if id_produto in estoque_produtos:
+            qtd_produto = int(input("Quantas unidades vcê deseja?"))
+            if qtd_produto <= 0:
+                print("Quantidade inválida!")
+            elif qtd_produto <= estoque_produtos[id_produto]["quantidade"]:
+                carrinho
+
+    elif opcao == 3:
+        print("Vizualizando carrinho!")
+
+    elif opcao == 4:
+        print("Finalizando compra!")
+
+    elif opcao == 5:
+        print("Saindo do sistema!")
+
+    else:
+        print("Opção inválida!!Tenta denovo beta")
